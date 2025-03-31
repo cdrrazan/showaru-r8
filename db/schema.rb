8f43dec7d785d60b5ffaa6b727578bb84fc4cdb8
Rajan Bhattarai <cdrrazan@gmail.com>
2025-03-31 22:13:06 +0545

    Run rem. migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_31_111329) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_31_111612) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_31_111329) do
   end
 
   create_table "settings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
