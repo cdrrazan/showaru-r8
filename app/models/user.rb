49b13b696d10d843b1d59c034eae0194f0705a30
Rajan Bhattarai <cdrrazan@gmail.com>
2025-04-03 17:34:43 +0545

    Setup Test App for Rails 8

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one :account
+  has_one :dashboard
+  has_one :subscription
+  has_many :projects
+end
